1f11c61a14e05af28de3ee91a47e0e133e883e09
mithrili <altnskihsan@gmail.com>
2024-02-06 13:36:48 +0300

    06/02/24

---
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class stepDefinition {
+    @Given("Establish a database connection.")
+    public void establish_a_database_connection() {
+
+    }
+    @Given("Query01 Prepare and execute the query.")
+    public void query01_prepare_and_execute_the_query() {
+
+    }
+    @Given("ResultSet01 Process the results.")
+    public void result_set01_process_the_results() {
+
+    }
+    @Given("Close the database connection.")
+    public void close_the_database_connection() {
+
+    }
+}
--- src/test/resources/Select.feature
+++ src/test/resources/Select.feature
@@ -2,3 +2,7 @@ Feature: SELECT QUERY EXECUTE
 
   Scenario:Validate the user_ids in the "deposits" table within
            the database for which the "amount" value is between $100 and $500.
+    * Establish a database connection.
+    * Query01 Prepare and execute the query.
+    * ResultSet01 Process the results.
+    * Close the database connection.
