b3b7385bcb1d6cb402a9c63a08cb2c8c6eb25360
mithrili <altnskihsan@gmail.com>
2024-02-06 11:39:26 +0300

    first commit

---
+++ src/main/java/JDBC_testing.java
@@ -0,0 +1,16 @@
+public class JDBC_testing {
+    /*
+    type: jdbc:mysql
+    host/ip: 45.87.83.5
+    port: 3306
+    database_name: u168183796_qaloantec
+    username: u168183796_qaloantecuser
+    password: 0&vG1A/MuWN
+        url = jdbc:mysql://45.87.83.5/u168183796_qaloantec
+        username: u168183796_qaloantecuser
+        password: 0&vG1A/MuWN
+     */
+    public static void main(String[] args) {
+
+    }
+}
