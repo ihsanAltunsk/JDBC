4f9c8e408dbaec664a0ee0a715e480dd0a2c2161
mithrili <altnskihsan@gmail.com>
2024-02-06 21:33:26 +0300

    06/02/24

--- src/test/java/manage/QueryManage.java
+++ src/test/java/manage/QueryManage.java
@@ -3,7 +3,7 @@ package manage;
     public class QueryManage {
 
 
-        private String query01 = "SELECT user_id FROM u168183796_qaloantec.deposits WHERE amount between 100 and 500;";
+        private String query01 = "SELECT user_id FROM u168183796_qaloantec.deposits WHERE amount between 100 and 500";
         private String query02 = "SELECT name FROM u168183796_qaloantec.cron_schedules Limit 2";
 
         //*********Getter**********
